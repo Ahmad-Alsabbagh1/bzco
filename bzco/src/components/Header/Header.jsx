85ae4494be68a54ab7dd3564c8556f7d3647e778
Ahmad-Alsabbagh1 <achmed.alsabbagh@gmail.com>
2023-09-14 14:42:04 +0200

    css changes to home page

--- bzco/src/components/Header/Header.jsx
+++ bzco/src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Header.css";
+import Navs from "./Navs";
 
 const Header = () => {
   return (
     <div className="header">
       <h1 className="name">BZ CO</h1>
+      <Navs />
       <div className="contact">
         <h4 className="number">+ 31 6 23 21 35 66</h4>
         <h4 className="email">info@bzco.nl</h4>
---
+++ bzco/src/components/Header/Navs.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navs = () => {
+  return (
+    <div className="navContainer">
+      <ul className="navs">
+        <li>Home</li>
+        <li>Diensten</li>
+        <li>Contact</li>
+        <li>Over Ons</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navs;
