6f3c62bd29dbbaa7000abcfca48ccfe3be7513e2
Ahmad-Alsabbagh1 <achmed.alsabbagh@gmail.com>
2023-09-20 15:12:10 +0200

    footer semi complete

--- bzco/src/components/Footer/Footer.jsx
+++ bzco/src/components/Footer/Footer.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import FooterContact from "./FooterContact";
 import "./footer.css";
+import logo from "../../assets/logo/bzcoLogo.jpg";
 const Footer = () => {
   return (
     <div className="footer">
       <h4 className="footerH4">
         Neem gerust contact met ons op om de mogelijkheden te bespreken
       </h4>
-      <FooterContact />
+
+      <div className="footerInfo">
+        <img src={logo} alt="bzco logo" className="bzco_logo" />
+        <FooterContact />
+      </div>
     </div>
   );
 };
